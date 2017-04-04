3ddf7cf72a3e4babc3eb1cb7910e412cd5ddba3b
Bauer Sandro <ga65bam@mytum.de>
2017-04-04 16:13:59 +0200

    First Successfull DB-Connection done.

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 
 // TODO: Implement CLI stuff here! (maybe)
 
-//var dbConnection = require("app/DBConnection.js");
-//dbConnection.
+var dbConnection = require('./app/DBConnection.js');
+var utils = require('./app/Utils.js');
+var content = utils.getFileContent('output/bach_output.json');
+var contentRel = utils.getFileContent('output/relationships.json');
 
-var wetFileParser = require("./app/WETParser.js");
+/*content.then(data => {
+  dbConnection.writeEvents(JSON.parse(data));
+});*/
+contentRel.then(data => {
+  dbConnection.writeRelationships(JSON.parse(data));
+});
 
-var url = "https://github.com/MusicConnectionMachine/UnstructuredData/files/850757/CC-MAIN-20170219104612-00150-ip-10-171-10-108.ec2.internal_filtered.warc.zip";
+var wetFileParser = require('./app/WETParser.js');
 
-wetFileParser.parse(url);
+var url = 'https://github.com/MusicConnectionMachine/UnstructuredData/files/850757/CC-MAIN-20170219104612-00150-ip-10-171-10-108.ec2.internal_filtered.warc.zip';
+
+// wetFileParser.parse(url);
--- app/DBConnection.js
+++ app/DBConnection.js
@@ -6,14 +6,90 @@ const api = require(apiLocation + "database.js");
 var context = null;
 
 function connect() {
-  if (!context) {
-    api.connect(function(localContext) {
-      context = localContext;
-    });
-  }
+  return new Promise(function(resolve) {
+    if (!context) {
+      api.connect(null, (localContext) => {
+        context = localContext;
+        resolve(context);
+      });
+    } else {
+      resolve(context);
+    }
+  });
+
 }
 
-module.exports.getAllEntities = function () {
-  connect();
-  //context.
+module.exports.getAllEntities = function() {
+  connect().then(() => {
+    var entities = context.component('models').module('entities');
+    return entities.findAll();
+  });
+};
+
+module.exports.writeRelationships = function(relationJSON) {
+  connect().then(() => {
+    var relationships = context.component('models').module('relationships');
+    //var relationshipDescription = context.component('models').module('relationshipDescriptions');
+    var relationshipEntities = context.component('models').module('relationshipEntities');
+    //var relationshipOccurrences = context.component('models').module('relationshipOccurrences');
+    //var relationshipTypes = context.component('models').module('relationshipTypes');
+
+    relationJSON.forEach((sentence) => {
+      sentence.instances.forEach((relation) => {
+        var subjectId = null;
+        var objectId = null;
+        //var relationshipId = null;
+        relationshipEntities.sync().then(() => {
+          if (relation.term1) {
+            return relationshipEntities.create({
+              'name': relation.term1
+            });
+          }
+        }).then(d => {
+          if (d) {
+            subjectId = d.id;
+          }
+          console.log(d);
+          if (relation.term2) {
+            return relationshipEntities.create({
+              'name': relation.term2
+            });
+          }
+        }).then(d => {
+          if (d) {
+            objectId = d.id;
+          }
+          return relationships.sync();
+        }).then(() => {
+          return relationships.create({
+            'confidence': relation.quality,
+            'subjectId': subjectId,
+            'objectId': objectId,
+            'relation': relation.relation
+          });
+        }).then(d => {
+          if (d) {
+            //relationshipId = d.id;
+          }
+        });
+      });
+    });
+    console.log('finished writing in DB');
+  });
+};
+
+module.exports.writeEvents = function(eventJSON) {
+  connect().then(() => {
+    var events = context.component('models').module('events');
+
+    eventJSON.forEach((event) => {
+      events.sync().then(() => {
+        events.create({
+          'start': event.start,
+          'end': event.end,
+          'description': event.event
+        });
+      });
+    });
+  });
 };
