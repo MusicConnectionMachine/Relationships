9bc08236aad6ea38a0a053313eac36cef31c3fe0
Simon Zachau <zachau@in.tum.de>
2017-04-04 17:07:22 +0200

    flattened promise chain in wet parser

--- app/WETParser.js
+++ app/WETParser.js
@@ -9,31 +9,28 @@ exports.parse = function(url) {
     .then(function(files) {
       console.log('finishedReading');
       // Just read the first file for now, TODO: read all files in the zip
-      utils.getFileContent(outputDir + '/' + files[0])
-        .catch(function (error) {
-          console.error(error);
-          console.log('error: reading file: ' + files[0]);
-          return;
-        }).then(function (data) {
-          // split the different websites
-          data = data.split("\n\n\n");
+      return utils.getFileContent(outputDir + '/' + files[0]);
+    }).catch(function (error) {
+      console.error('error: ' + error + '; reading file: ' + files[0]);
+      return;
+    }).then(function (data) {
+      // split the different websites
+      data = data.split("\n\n\n");
 
-          // filter out warc info, TODO: we may need that data later
-          // TODO: use more than one website (still testing)
-          var content = [];
-          data.forEach(function(entry) {
-            content.push(entry.split("\n\n")[1]);
-          });
+      // filter out warc info, TODO: we may need that data later
+      // TODO: use more than one website (still testing)
+      var content = [];
+      data.forEach(function(entry) {
+        content.push(entry.split("\n\n")[1]);
+      });
 
-          // TODO: delete this later, just try to prevent timeout
-          //content = content.substring(0, 100);
-          console.log('Website Count: ' + content.length);
+      // TODO: delete this later, just try to prevent timeout
+      //content = content.substring(0, 100);
+      console.log('Website Count: ' + content.length);
 
-          // TODO: Call Coref and other algorithms
-          return content;
-        });
-    })
-    .catch( e => console.error('error while downloading', e));
+      // TODO: Call Coref and other algorithms
+      return content;
+    }).catch(function(e) {
+      console.error('error while downloading', e);
+    });
 };
-
-
