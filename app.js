079c229542533e3149a76904b3b0eaef389cd868
Bauer Sandro <ga65bam@mytum.de>
2017-04-04 17:57:00 +0200

    Simplify code

--- app.js
+++ app.js
@@ -7,17 +7,14 @@ const wetFileParser = require('./app/wetParser.js');
 const dbConnection = require('./app/dbConnection.js');
 
 // test writing event data in DB
-let content = utils.getFileContent('output/bachOutput.json');
-content.then(data => {
+utils.getFileContent('output/bachOutput.json').then(data => {
   dbConnection.writeEvents(JSON.parse(data));
 });
 
 // test writing relationship data in DB
-let contentRel = utils.getFileContent('output/relationships.json');
-contentRel.then(data => {
+utils.getFileContent('output/relationships.json').then(data => {
   dbConnection.writeRelationships(JSON.parse(data));
 });
 
 // test getting and parsing a wet file from the url
-let url = 'https://github.com/MusicConnectionMachine/UnstructuredData/files/850757/CC-MAIN-20170219104612-00150-ip-10-171-10-108.ec2.internal_filtered.warc.zip';
-wetFileParser.parse(url);
+wetFileParser.parse('https://github.com/MusicConnectionMachine/UnstructuredData/files/850757/CC-MAIN-20170219104612-00150-ip-10-171-10-108.ec2.internal_filtered.warc.zip');
