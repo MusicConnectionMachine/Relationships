be5d9ac46dd232e726d84e2ca2a9bac86d04953e
Anshul Jindal <anshul.jindal@tum.de>
2017-04-07 22:57:01 +0200

    updated Date_Event_Extraction_Algorithm

--- algorithms/date_event_extraction/scripts/deploy.sh
+++ algorithms/date_event_extraction/scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 echo "Deploying the dateEventExtraction Algorithm in seprate container"
 cd ..
-echo "Step 1: Building Docker Image"
-docker build -t dateEventExtraction .
-echo "Step 2: Running Docker Image"
-(docker run -p 3003:3003 dateEventExtraction)&
-read -p "Press enter to continue"
+echo "Step 1: Building and Running Docker Image"
+docker-compose up --build
+read -p "Press [Enter] key to End..."
