b928e0cfed115e270bc874d104aa452cfad60fa7
René Birkeland <renesbirkeland@gmail.com>
2017-04-08 10:54:49 +0200

    Temp commit to change computer

--- app/classification.js
+++ app/classification.js
@@ -1,20 +1,89 @@
 'use strict';
 
+const natural = require('natural');
+const wordnet = new natural.WordNet();
+
 var teacherArray = ['taught','coached', 'trained', 'educated'];
 var studentArray = ['learned from'];
 var wroteArray = ['wrote','created', 'composed'];
 var playedArray = ['played','performed'];
 
-var dict = {teacher:teacherArray, student:studentArray, wrote:wroteArray, played:playedArray};
+const dict = {teacher:teacherArray, student:studentArray, wrote:wroteArray, played:playedArray};
+
+var boo = false;
 
 module.exports.findRelationshipClass = function(word) {
-  for(var key in dict) {
-	var list = dict[key];
-	for(var w in list) {
-	  if(list[w] === word) {
-	    return key;
-		}
-	  }
-	}
-	return null;
+  word = word.toLowerCase();
+  for(let key in dict) {
+    let list = dict[key];
+    for(let w in list) {
+      if(list[w] === word) {
+        return key;
+      }
+    }
+  }
+  return null;
 };
+
+function populateArray(array, word) {
+  return new Promise(function (resolve) {
+    wordnet.lookup(word, function (results) {
+      results.forEach(function (result) {
+        for (let value in result.synonyms) {
+          array.push(result.synonyms[value]);
+        }
+      });
+      array = Array.from(new Set(array));
+      //console.log("TEST"+array);
+      resolve(array);
+    });
+  }).catch(function() {
+    console.log('Promise Rejected');
+  });
+}
+
+function init(){
+  for(let key in dict) {
+    populateArray(dict[key], key);
+  }
+}
+
+init();
+
+
+/*populateArray(teacherArray, 'teach').then(() => {
+  //console.log(teacherArray)
+});
+populateArray(studentArray, 'student').then(() => {
+  //console.log(studentArray)
+});
+populateArray(wroteArray, 'write').then(() => {
+  //console.log(wroteArray)
+});
+populateArray(playedArray, 'perform').then(() => {
+  //console.log(playedArray)
+}).then(function() {
+  const classifier = new natural.BayesClassifier();
+
+  for(var value in teacherArray) {
+    classifier.addDocument(value, 'teach');
+  }
+  for(var value in studentArray) {
+    classifier.addDocument(value, 'student');
+  }
+  for(var value in wroteArray) {
+    classifier.addDocument(value, 'wrote');
+  }
+  for(var value in playedArray) {
+    classifier.addDocument(value, 'perform');
+  }
+  classifier.train();
+
+  console.log(playedArray);
+
+  console.log(classifier.classify('do'));
+
+  console.log('class: '+ classifier.getClassifications('do'));
+
+});
+*/
