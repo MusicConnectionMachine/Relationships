c91335a4fb2d9a1266b0c4b7b3de20e53e433ef1
René Birkeland <renesbirkeland@gmail.com>
2017-04-08 11:09:12 +0200

    corrected

--- app/classification.js
+++ app/classification.js
@@ -1,6 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
 const natural = require('natural');
 const wordnet = new natural.WordNet();
 
@@ -8,20 +7,16 @@ var teacherArray = ['taught','coached', 'trained', 'educated'];
 var studentArray = ['learned from'];
 var wroteArray = ['wrote','created', 'composed'];
 var playedArray = ['played','performed'];
-=======
+
 const teacherArray = ['taught','coached', 'trained', 'educated'];
 const studentArray = ['learned from'];
 const wroteArray = ['wrote','created', 'composed'];
 const playedArray = ['played','performed'];
->>>>>>> bd77b7899c4d1a719205fbe45eb1d6ddd32c240e
 
 const dict = {teacher:teacherArray, student:studentArray, wrote:wroteArray, played:playedArray};
 
 module.exports.findRelationshipClass = function(word) {
-<<<<<<< HEAD
   word = word.toLowerCase();
-=======
->>>>>>> bd77b7899c4d1a719205fbe45eb1d6ddd32c240e
   for(let key in dict) {
     let list = dict[key];
     for(let w in list) {
