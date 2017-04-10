58d12fcf45d5d39d08b0baae0d26e6b19e8f91eb
Gillian0023 <jilin0023@gmail.com>
2017-04-10 17:44:01 +0200

    add small stuff about envconfig and dbconnection

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const commander = require('commander');
+const pgdb=require('./example/fillDatabase.js');
+const wetparser=require('./app/wetParser.js');
 
 function collect(val, memo) {
   memo.push(val);
@@ -16,7 +18,7 @@ commander
   .parse(process.argv);
 
 const defaultConfig = {
-  dbHost: '172.0.0.',
+  dbHost: '172.0.0.1',
   dbPort: 5432,
   dbUser: 'postgres',
   dbPassword: 'NotRequired',
@@ -25,15 +27,45 @@ const defaultConfig = {
   blobKey: 'unknown',
 };
 
-const envConfig = {
+const relExtraction=commander.RelExtraction||process.env.r;
+const coRef=commander.CoRef||process.env.c;
+const dateExtraction=commander.DateExtraction||process.env.a;
 
+
+if(commander.db){
+  pgdb.populateDB();
+}
+if(commander.blobKey){
+  wetparser.parse();
+}
+
+const envConfig = {
+  relAlgorithms:[],
+  host:[],
+  port:[],
+  path:[]
 };
 
-const db = commander.blobLocation.split(":", 2);
 
+if(relExtraction=='Ollie'){
+  envConfig.push({relAlgorithm:'Ollie',host:'localhost', port:'3000'});
+  console.log('running Ollie');
+}
+if(relExtraction=='OpenieWashington'){
+  envConfig.push({host:'localhost',port:'3001'});
+  console.log('running OpenWashington');
+}
+if(relExtraction=='OpenieStandardford'){
+  envConfig.push({host:'localhost',port:'3002'});
+  console.log('running OpenStandford');
+}
+
+
+
+
+//const db = commander.blobLocation.split(":", 2);
 const paramConfig = {
   dbUri: commander.db,
-
 };
 
 const finalConfig = Object.assign({}, defaultConfig, envConfig, paramConfig);
