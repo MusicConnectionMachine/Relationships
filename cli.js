24b9208d61eb3f170b2f83eccc4fda6d55b4c010
Hendrik Leppelsack <hendrik@leppelsack.de>
2017-04-11 16:09:27 +0200

    start implementing the main pipeline

--- cli.js
+++ cli.js
@@ -2,16 +2,34 @@
 
 const config = require('./app/config');
 const db = require('./app/dbConnection');
+const wetParser = require('./app/wetParser.js');
 const algorithms = require('./app/algorithms');
 
 
 db.getWebsitesToEntities()
-  .then(
-    (websites) => algorithms.call(websites)
-  )
+  .then((blobPerEntity) => {
+    return Promise.all(
+      Object.keys(blobPerEntity).map(
+        entity => handleEntityBlobs(blobPerEntity[entity])
+      )
+    );
+  })
   .then(
     () => console.log('TODO: dump to database')
   )
   .then(
     () => console.log('done')
   );
+
+
+function handleEntityBlobs(blobs) {
+  return Promise.all(
+    blobs.map(blobUrl => handleBlob(blobUrl))
+  );
+
+}
+
+function handleBlob(blobUrl) {
+  return wetParser.parse(blobUrl)
+    .then(algorithms.call);
+}
